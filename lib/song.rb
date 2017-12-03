2c4cbca1b3797f2fae8abe11e97e6521d94b9a54
Matthew Hunger <matt.hunger@gmail.com>
2017-12-03 11:47:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
   end
 
   def self.create
-    return song = song.save #or is it Song.save? or self.save
+    return song = Song.save #or is it Song.save? or self.save
   end
 
   def self.new_by_name(song)
