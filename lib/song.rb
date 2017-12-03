2e7ec30bd90bba8ff9e9008378df625def076062
Matthew Hunger <matt.hunger@gmail.com>
2017-12-03 11:32:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,30 @@ class Song
     self.class.all << self
   end
 
+# do I need to initialize??????????
+  def initialize(name)
+    @name = name
+    @artist = artist
+  end
+
+  def self.create
+    return Song.save #or is it self.save? 
+  end
+
+  def self.new_by_name(song)
+
+
+=begin
+    @@all.each do |t|
+      if t.name == song
+        return t
+      end
+    end
+=end
+  end
+
+  def self.create_by_name
+
+  end
+
 end
