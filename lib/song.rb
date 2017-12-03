045d12f83f5f7d9f60e354641654800abbbabf78
Matthew Hunger <matt.hunger@gmail.com>
2017-12-03 12:08:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,6 +45,8 @@ class Song
   def self.find_or_create_by_name(song)
     if self.find_by_name(song) == nil
         self.create_by_name(song)
+    else
+      return self.find_by_name(song)
     end
   end
 
